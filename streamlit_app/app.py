451e42c0a9fb5431f4bf38b002525692bd91717f
NishthaNishtha <nishtha100.30singh@gmail.com>
2026-05-02 12:13:01 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -6,9 +6,9 @@ import os
 import joblib
 
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, "random_forest_model.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "random_forest_model.pkl")
 
-model = joblib.load(model_path)
+model = joblib.load(MODEL_PATH)
 
 st.title("Hotel Booking Cancellation Prediction System")
 
